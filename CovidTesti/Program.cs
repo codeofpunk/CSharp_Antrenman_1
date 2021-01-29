40030ae414ca80ca05d22fd12361104cf996190a
Batuhan <batuhan@kasifdijital.com>
2021-01-30 00:52:51 +0300

    Add project files.

---
+++ CovidTesti/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidTesti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            BaslikGetir();
+            Console.Write("Yaşınız: ");
+            string yas = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Ateşin var mı? " + SoruEk());
+            string atesCevap = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Öksürük var mı? " + SoruEk());
+            string oksurukCevap = Convert.ToString(Console.ReadLine());
+
+            if (atesCevap == "e" || oksurukCevap == "e" || atesCevap == "h" || oksurukCevap == "h")
+            {
+              
+                 if (atesCevap == "e" && oksurukCevap == "e")
+                {
+
+                    Console.WriteLine("Covid Oldun.");
+
+                }
+
+                else
+                {
+                    if (atesCevap == "e")
+                    {
+                        Console.WriteLine("Ateşiniz olduğu için büyük risk altındasınız.");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Covid deelsin.");
+                    }
+
+                }
+            }
+
+            else
+            {
+
+                Console.WriteLine("HATALI KOD");
+
+            }
+            Console.ReadLine();
+        }
+
+        static void BaslikGetir()
+        {
+            Console.WriteLine("COVID 19 TEST");
+        }
+
+        static string SoruEk()
+        {
+           string Metin = "Var ise(e) yok ise(h)";
+           return Metin;
+        }
+    }
+}
---
+++ Çalışma1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Çalışma1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi1,sayi2,sonuc = 0;
+            string islem;
+
+            Console.WriteLine("Hesap Makinesi");
+
+            Console.Write("1. Sayı: ");
+            sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("2. Sayı: ");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Yapmak istediğiniz işlem: (*, +, -, /)");
+            islem = Convert.ToString(Console.ReadLine());
+
+            if (islem == "*" || islem == "+" || islem == "-" || islem == "/")
+            {
+
+                if (islem == "*")
+                {
+                    sonuc = sayi1 * sayi2;
+                    
+                }
+
+                else if (islem == "+")
+                {
+                    sonuc = sayi1 + sayi2;
+                  
+                }
+
+                else if (islem == "-")
+                {
+                    sonuc = sayi1 - sayi2;
+                   
+                }
+
+                else if (islem == "/")
+                {
+                    sonuc = sayi1 / sayi2;
+                    
+                }
+
+                Console.WriteLine("İşlem Sonucunuz: " + sonuc.ToString());
+
+            }
+
+            else
+            {
+                Console.WriteLine("Hatalı işlem kodu!");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
